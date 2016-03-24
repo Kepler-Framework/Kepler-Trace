cb9e2661ec82d5cc0ebf14e6a959dbead8f673eb
BluesZhang <zhangjiehao@didichuxing.com>
2016-03-24 19:47:25 +0800

    重构项目结构 并 优化

--- kepler-trace-interface/src/main/java/com/kepler/trace/collector/TraceTransferService.java
+++ kepler-trace-interface/src/main/java/com/kepler/trace/collector/TraceTransferService.java
@@ -1,12 +1,10 @@
 package com.kepler.trace.collector;
 
-import com.kepler.annotation.Async;
 import com.kepler.annotation.Service;
 
 @Service(version="0.0.1")
 public interface TraceTransferService {
 
-	@Async
 	void transferTraceInfos(TraceInfos traceInfos);
 	
 }
---
+++ kepler-trace-service/src/test/java/com/kepler/trace/collector/impl/TraceTransferServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.kepler.trace.collector.impl;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class TraceTransferServiceImplTest {
+
+	@Test
+	public void test() {
+		long timestamp = 1458723029124L;
+		Date date = new Date(timestamp);
+		System.out.println(date);
+	}
+
+}
