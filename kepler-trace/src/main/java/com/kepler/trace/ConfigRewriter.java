3448355d941e9676b8af2555a8e7320e85973ea2
BluesZhang <zhangjiehao@didichuxing.com>
2016-05-31 10:13:15 +0800

    fix bug

--- kepler-trace/src/main/java/com/kepler/trace/ConfigRewriter.java
+++ kepler-trace/src/main/java/com/kepler/trace/ConfigRewriter.java
@@ -1,17 +1,12 @@
 package com.kepler.trace;
 
-import java.util.Map;
-
-import com.kepler.config.PropertiesUtils;
 import com.kepler.serial.SerialID;
 import com.kepler.trace.collector.TraceTransferService;
 
 public class ConfigRewriter {
 
 	public ConfigRewriter() {
-		Map<String, String> properties = PropertiesUtils.memory();
-		properties.put(TraceTransferService.class.getName().toLowerCase() + "." + SerialID.class.getName().toLowerCase() + ".serial", "snappy");
-		PropertiesUtils.properties(properties);
+		System.setProperty(TraceTransferService.class.getName().toLowerCase() + "." + SerialID.class.getName().toLowerCase() + ".serial", "snappy");
 	}
 
 }
